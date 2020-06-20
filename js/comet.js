b6eb9e13c8f70d68a1fe10ff4bbf6b9fb3e5f087
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-21 01:51:45 +0200

    comet

--- js/comet.js
+++ js/comet.js
@@ -57,7 +57,7 @@ Particle.prototype = {
 // ----------------------------------------
 
 var MAX_PARTICLES = 280;
-var COLOURS = [ '#f2ab41' ];
+var COLOURS = [ '#f2ab41' , '#192065' ];
 
 var particles = [];
 var pool = [];
@@ -87,14 +87,14 @@ demo.spawn = function( x, y ) {
         pool.push( particles.shift() );
 
     particle = pool.length ? pool.pop() : new Particle();
-    particle.init( x, y, random( 50, 40 ) );
+    particle.init( x, y, random( 15, 60 ) );
 
     particle.wander = random( 0.5, 2.0 );
     particle.color = random( COLOURS );
     particle.drag = random( 0.9, 0.99 );
 
     theta = random( TWO_PI );
-    force = random( 0,0 );
+    force = random( 2, 5 );
 
     particle.vx = sin( theta ) * force;
     particle.vy = cos( theta ) * force;
