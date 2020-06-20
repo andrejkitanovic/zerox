34b19be56a0eb2071db1f0ac9a545c516f559080
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-21 01:05:20 +0200

    ch

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ const drawBackground = () => {
 };
 const loop = () => {
 	drawBackground();
-	const sz = (Math.min(canvas.width, canvas.height) / 3) * 2;
+	const sz = (Math.min(canvas.width, canvas.height) / 3) * 1.8;
 	drawGradient(sz);
 	drawLines(sz);
 	t += 0.5;
