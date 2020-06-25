a899774a635aba9c061367f0fc00992e30bfd9c3
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-06-25 04:33:12 +0200

    ch

---
+++ js/swiper.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 30,
+      stretch: 0,
+      depth: 500,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
